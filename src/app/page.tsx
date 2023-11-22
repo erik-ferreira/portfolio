59885a348cf9ef4a56a9c195ba2473ffa46bec56
erik-ferreira <erikdesouzagf@gmail.com>
2023-11-22 19:15:40 -0300

    create component TechIcon; add icons techs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,15 @@
-import { Title } from "@/components/Title"
-
 import { Header } from "@/components/page/Header"
 import { Resume } from "@/components/page/Resume"
+import { Skills } from "../components/page/Skills"
 
 export default function Home() {
   return (
     <main>
       <Header />
 
-      <Resume />
+      {/* <Resume /> */}
 
-      <div className="flex items-center justify-center gap-6 p-8">
-        <Title />
-      </div>
+      <Skills />
     </main>
   )
 }
---
+++ src/components/TechIcon.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image"
+import { ComponentProps } from "react"
+
+import { twMerge } from "@/utils/twMerge"
+
+import reactSVG from "@/assets/techs/react.svg"
+
+interface TechIconProps extends ComponentProps<"div"> {}
+
+export function TechIcon({ className, ...rest }: TechIconProps) {
+  return (
+    <div
+      className={twMerge(
+        "flex flex-col items-center justify-center relative group technologies-item",
+        className
+      )}
+      {...rest}
+    >
+      <Image
+        src={reactSVG}
+        alt="Ícone da tecnologia React"
+        width={100}
+        height={100}
+        className="h-[100px] transition duration-300 grayscale hover:grayscale-0 group-hover:transform group-hover:-translate-y-4 z-10"
+      />
+      <p className="opacity-0 transition duration-300 absolute -bottom-4 group-hover:opacity-100 z-10">
+        Firebase
+      </p>
+    </div>
+  )
+}
