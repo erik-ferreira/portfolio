58b4a7012dac494260c487040534323ee2238a94
erik-ferreira <erikdesouzagf@gmail.com>
2023-11-24 23:08:27 -0300

    complete timeline with lib

--- src/components/page/Experiences.tsx
+++ src/components/page/Experiences.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { User } from "lucide-react"
+import Image from "next/image"
 import {
   VerticalTimeline,
   VerticalTimelineElement,
@@ -17,9 +18,9 @@ interface ExperiencesProps {}
 export function Experiences({ ...rest }: ExperiencesProps) {
   return (
     <div className="max-w-content w-content mx-auto flex flex-col items-center gap-6 p-8">
-      <Title label="Experiência" labelBackground="Experience" />
-      ok here
-      <VerticalTimeline className="border">
+      {/* <Title label="Experiência" labelBackground="Experience" /> */}
+
+      <VerticalTimeline lineColor="#0284c7">
         {experiences.map((experience) => (
           <VerticalTimelineElement
             key={experience.id}
@@ -30,14 +31,37 @@ export function Experiences({ ...rest }: ExperiencesProps) {
               borderRadius: 4,
               borderWidth: 1,
               borderColor: "#0284c7",
+              boxShadow: `0px 0px 0px 0px`,
+              display: "flex",
+              flexDirection: "column",
+              gap: "4px",
             }}
             contentArrowStyle={{ borderRight: "7px solid #0284c7" }}
             date={experience.date}
+            icon={
+              <Image
+                src={experience.image}
+                width={70}
+                height={70}
+                alt="icon experience"
+              />
+            }
+            iconStyle={{
+              boxShadow: `0px 0px 0px 0px #0284c7`,
+              background: "#0284c7",
+            }}
+            dateClassName="text-slate-300"
           >
-            <h3>{experience.office}</h3>
-            <p>{experience.company}</p>
+            <h3 className="text-3.5xl leading-snug text-sky-600 font-bold">
+              {experience.office}
+            </h3>
+            <h4 className="text-2xl text-violet-300 font-bold">
+              {experience.company}
+            </h4>
 
-            <span>{experience.description}</span>
+            <span className="w-full text-slate-300">
+              {experience.description}
+            </span>
           </VerticalTimelineElement>
         ))}
       </VerticalTimeline>
--- src/defaults/experiences.ts
+++ src/defaults/experiences.ts
@@ -6,13 +6,15 @@ export const experiences = [
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
     date: "Agosto 2019 - Dezembro 2019",
+    image: "/experiences/softtek.png",
   },
   {
     id: 2,
     office: "Desenvolvedor Frontend",
     company: "Tecla T",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500.",
+      "Lorem Ipsum is simply dummy text of the printing and typetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500.",
     date: "Agosto 2019 - Dezembro 2019",
+    image: "/experiences/softtek.png",
   },
 ]
