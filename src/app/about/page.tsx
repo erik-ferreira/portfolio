da3e7259750b9f8514cbd3c82b80e24f24762d3e
erik-ferreira <erikdesouzagf@gmail.com>
2023-11-29 15:46:18 -0300

    start structure by page About

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,78 @@
+import Link from "next/link"
+import Image from "next/image"
+
+import { Icon } from "@/components/Icon"
+import { Title } from "@/components/Title"
+import { Button } from "@/components/Button"
+
+import erikPNG from "@/assets/erik.png"
+import { SocialIcons } from "@/components/SocialIcons"
+
 export default function About() {
   return (
     <main className="relative max-w-content w-content mx-auto p-8 flex flex-col gap-16 border">
-      <section>About</section>
+      <section>
+        <Title label="Sobre" labelBackground="About" />
+        <p>Uma breve descrição aqui</p>
+
+        <article className="max-w-[932px] mx-auto border flex gap-8">
+          <div className="border max-w-[350px] w-full flex flex-col items-center gap-4">
+            <Image
+              src={erikPNG}
+              width={350}
+              height={350}
+              alt="Erik Ferreira"
+              className="rounded-lg"
+            />
+
+            <div className="w-full h-0.5 bg-sky-600" />
+
+            <SocialIcons variant="completed" />
+
+            <div className="w-full h-0.5 bg-sky-600" />
+
+            <Button variant="outline-gradient" asChild>
+              <Link href="/">
+                Entre em contato
+                <Icon size="small" disabledHover />
+              </Link>
+            </Button>
+          </div>
+
+          <span className="flex-1">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry standard dummy text ever
+            since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. Lorem Ipsum is simply dummy text of
+            the printing and typesetting industry. Lorem Ipsum has been the
+            industry standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the
+            leap into electronic typesetting, remaining essentially unchanged.
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry standard dummy text ever
+            since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. Lorem Ipsum is simply dummy text of
+            the printing and typesetting industry. Lorem Ipsum has been the
+            industry standard dummy text ever since the 1500s, when an unknown
+            printer took a galley of type and scrambled it to make a type
+            specimen book. It has survived not only five centuries, but also the
+            leap into electronic typesetting, remaining essentially unchanged.
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry standard dummy text ever
+            since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. Lorem Ipsum is simply dummy text of
+            the printing and typesetting industry. Lorem Ipsum has been the
+            industry standard dummy text ever since the 1500.
+          </span>
+        </article>
+      </section>
     </main>
   )
 }
--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -5,14 +5,30 @@ import { tv, VariantProps } from "tailwind-variants"
 import { Icon } from "@/components/Icon"
 import { IconSvgExternal } from "@/components/IconSvgExternal"
 
+import { socialIconsList } from "@/defaults/socialIcons"
+
 const socialIcons = tv({
-  base: "flex items-center gap-6",
+  slots: {
+    base: "flex items-center gap-6",
+    link: "",
+    icon: "",
+    iconExternal: "",
+  },
 
   variants: {
     variant: {
-      default: "",
-      fixed:
-        "flex-col fixed bottom-0 left-14 z-10 after:content-normal after:w-1 after:h-[150px] after:bg-blue-600 after:rounded-lg",
+      default: {
+        base: "",
+      },
+      fixed: {
+        base: "flex-col fixed bottom-0 left-14 z-10 after:content-normal after:w-1 after:h-[150px] after:bg-blue-600 after:rounded-lg",
+      },
+      completed: {
+        base: "flex-col items-start gap-2",
+        link: "flex gap-1 items-center text-lg font-semibold hover:text-sky-600 group",
+        icon: "group-hover:text-sky-600",
+        iconExternal: "group-hover:fill-sky-600",
+      },
     },
   },
 
@@ -25,20 +41,31 @@ type SocialIconsProps = VariantProps<typeof socialIcons> &
   ComponentProps<"div"> & {}
 
 export function SocialIcons({ variant, className, ...rest }: SocialIconsProps) {
+  const { base, link, icon, iconExternal } = socialIcons({ variant, className })
+
   return (
-    <div className={socialIcons({ variant, className })} {...rest}>
-      <Link href="/" prefetch={false}>
-        <Icon name="Github" />
-      </Link>
-      <Link href="/" prefetch={false}>
-        <Icon name="Instagram" />
-      </Link>
-      <Link href="/" prefetch={false}>
-        <Icon name="Linkedin" />
-      </Link>
-      <Link href="/" prefetch={false}>
-        <IconSvgExternal type="WhatsApp" />
-      </Link>
+    <div className={base()} {...rest}>
+      {socialIconsList.map((socialIcon) => {
+        return (
+          <Link
+            key={socialIcon.id}
+            href={socialIcon.href}
+            prefetch={false}
+            className={link()}
+          >
+            {socialIcon.type === "default" ? (
+              <Icon name={socialIcon.name} className={icon()} />
+            ) : (
+              <IconSvgExternal
+                type={socialIcon.externalIcon}
+                className={iconExternal()}
+              />
+            )}
+
+            {variant === "completed" && socialIcon.label}
+          </Link>
+        )
+      })}
     </div>
   )
 }
