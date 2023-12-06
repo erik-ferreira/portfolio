edaae2296cba795cf707803ef6d7a15d934da82a
erik-ferreira <erikdesouzagf@gmail.com>
2023-12-05 23:16:40 -0300

    create list projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,12 +1,6 @@
-import Image from "next/image"
-import NextLink from "next/link"
-
-import { Link } from "@/components/Link"
-import { Icon } from "@/components/Icon"
 import { Title } from "@/components/Title"
 import { Input } from "@/components/Input"
-import { Divider } from "@/components/Divider"
-import { TechName } from "@/components/TechName"
+import { Project } from "@/components/Project"
 import { Description } from "@/components/Description"
 
 import { listAllProjects } from "@/defaults/projects"
@@ -24,52 +18,9 @@ export default function Projects() {
           classNameContainer="mx-auto"
         />
 
-        <article className="flex flex-col gap-8 mt-8">
+        <article className="flex flex-col mt-8 divide-y-2 divide-sky-600 border-t border-sky-600">
           {listAllProjects.map((project) => (
-            <>
-              <Divider />
-
-              <div key={project.id} className="flex gap-12">
-                <Image
-                  src={project.src}
-                  alt="Project"
-                  width={500}
-                  height={333}
-                />
-
-                <div className="flex flex-col justify-between">
-                  <div className="flex items-center justify-between">
-                    <h3 className="text-4.75xl font-bold">{project.title}</h3>
-
-                    <div className="flex items-center gap-2">
-                      <NextLink href="/">
-                        <Icon name="Github" size="large" />
-                      </NextLink>
-                      <NextLink href="/">
-                        <Icon name="Link" size="large" />
-                      </NextLink>
-                    </div>
-                  </div>
-
-                  <p className="text-lg font-semibold text-slate-500">
-                    {project.description}
-                  </p>
-
-                  <ul className="flex items-center gap-2 flex-wrap">
-                    {project.techs.map((tech) => (
-                      <TechName key={tech} name={tech} />
-                    ))}
-                  </ul>
-
-                  <Link
-                    href="/"
-                    label="Ler mais"
-                    className="text-slate-500"
-                    size="large"
-                  />
-                </div>
-              </div>
-            </>
+            <Project key={project.id} project={project} />
           ))}
         </article>
       </section>
---
+++ src/components/Project.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image"
+import NextLink from "next/link"
+import { ComponentProps } from "react"
+
+import { Link } from "@/components/Link"
+import { Icon } from "@/components/Icon"
+import { TechName } from "@/components/TechName"
+
+import { twMerge } from "@/utils/twMerge"
+
+interface ProjectDTO {
+  id: number
+  src: string
+  title: string
+  description: string
+  techs: Array<string>
+}
+
+interface ProjectProps extends ComponentProps<"div"> {
+  project: ProjectDTO
+}
+
+export function Project({ project, className, ...rest }: ProjectProps) {
+  return (
+    <div className={twMerge("flex gap-12 py-8", className)} {...rest}>
+      <Image src={project.src} alt="Project" width={500} height={333} />
+
+      <div className="flex flex-col justify-between">
+        <div className="flex items-center justify-between">
+          <h3 className="text-4.75xl font-bold">{project.title}</h3>
+
+          <div className="flex items-center gap-2">
+            <NextLink href="/">
+              <Icon name="Github" size="large" />
+            </NextLink>
+            <NextLink href="/">
+              <Icon name="Link" size="large" />
+            </NextLink>
+          </div>
+        </div>
+
+        <p className="text-lg font-semibold text-slate-500">
+          {project.description}
+        </p>
+
+        <ul className="flex items-center gap-2 flex-wrap">
+          {project.techs.map((tech) => (
+            <TechName key={tech} name={tech} />
+          ))}
+        </ul>
+
+        <Link
+          href="/"
+          label="Ler mais"
+          className="text-slate-500"
+          size="large"
+        />
+      </div>
+    </div>
+  )
+}
