731d3192af8c533a6c1f22a46d0e0530cfbe8d1d
erik-ferreira <erikdesouzagf@gmail.com>
2023-12-06 23:52:59 -0300

    create component Skill

---
+++ src/components/Skill.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+
+interface SkillProps {}
+
+export function Skill({ ...rest }: SkillProps) {
+  return (
+    <div className="w-fit flex flex-col items-center gap-4">
+      <div className="w-[100px] h-[100px] rounded-full bg-[#007ACC]/10 border-[1.5px] border-[#007ACC] flex items-center justify-center">
+        <Image
+          src="/skills/typescript.svg"
+          alt="Typescript"
+          width={60}
+          height={60}
+          className="rounded-lg"
+        />
+      </div>
+
+      <span className="text-2xl font-bold text-[#007ACC]">Typescript</span>
+    </div>
+  )
+}
