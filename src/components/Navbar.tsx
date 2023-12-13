d651f4cf4727701a80f81a618a515a7ab00a55f5
erik-ferreira <erikdesouzagf@gmail.com>
2023-12-13 19:28:40 -0300

    add links to navigate in NavBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { usePathname } from "next/navigation"
 import * as NavigationMenu from "@radix-ui/react-navigation-menu"
 
 import { Link } from "@/components/Link"
@@ -9,14 +10,22 @@ import { listOptionsNavBar } from "@/defaults/navbar"
 interface NavbarProps extends NavigationMenu.NavigationMenuProps {}
 
 export function Navbar({ ...rest }: NavbarProps) {
+  const pathname = usePathname()
+
   return (
     <NavigationMenu.Root {...rest}>
       <NavigationMenu.List className="flex items-center gap-8">
         {listOptionsNavBar.map((item) => {
+          const isLinkSelected = pathname.includes(item.href)
           return (
             <NavigationMenu.Item key={item.id}>
               <NavigationMenu.Link asChild>
-                <Link href={item.href} label={item.label} hideIcon />
+                <Link
+                  href={item.href}
+                  label={item.label}
+                  hideIcon
+                  variant={isLinkSelected ? "selected" : "default"}
+                />
               </NavigationMenu.Link>
             </NavigationMenu.Item>
           )
--- src/defaults/navbar.ts
+++ src/defaults/navbar.ts
@@ -1,9 +1,9 @@
 import { NavBarDTO } from "@/dtos/NavBarDTO"
 
 export const listOptionsNavBar: NavBarDTO[] = [
-  { id: 1, label: "Sobre", href: "/" },
-  { id: 2, label: "Projetos", href: "/" },
-  { id: 3, label: "Experiência", href: "/" },
-  { id: 4, label: "Resumo", href: "/" },
-  { id: 5, label: "Contato", href: "/" },
+  { id: 1, label: "Sobre", href: "/about" },
+  { id: 2, label: "Projetos", href: "/projects" },
+  { id: 3, label: "Certificados", href: "/certifications" },
+  { id: 4, label: "Resumo", href: "/resume" },
+  { id: 5, label: "Contato", href: "/contact" },
 ]
