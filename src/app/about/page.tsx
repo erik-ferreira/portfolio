8046c3d97acda51060475bb4bbae81b9c9ea5766
erik-ferreira <erikdesouzagf@gmail.com>
2024-01-10 10:45:23 -0300

    change text about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,7 +14,7 @@ export default function About() {
   return (
     <main className="max-w-content w-content mx-auto p-8">
       <Title label="Sobre" labelBackground="About" isTitlePage />
-      <Description label="Uma breve descrição aqui" />
+      <Description label="Um pouco sobre mim" />
 
       <article className="max-w-[932px] mx-auto flex gap-8">
         <div className="max-w-[350px] w-full flex flex-col items-center gap-4">
@@ -40,67 +40,64 @@ export default function About() {
           </Button>
         </div>
 
-        <span className="flex-1">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. Lorem Ipsum is simply dummy text of
-          the printing and typesetting industry. Lorem Ipsum has been the
-          industry standard dummy text ever since the 1500s, when an unknown
-          printer took a galley of type and scrambled it to make a type specimen
-          book. It has survived not only five centuries, but also the leap into
-          electronic typesetting, remaining essentially unchanged. Lorem Ipsum
-          is simply dummy text of the printing and typesetting industry. Lorem
-          Ipsum has been the industry standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it to make
-          a type specimen book. It has survived not only five centuries, but
-          also the leap into electronic typesetting, remaining essentially
-          unchanged. Lorem Ipsum is simply dummy text of the printing and
-          typesetting industry. Lorem Ipsum has been the industry standard dummy
-          text ever since the 1500s, when an unknown printer took a galley of
-          type and scrambled it to make a type specimen book. It has survived
-          not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. Lorem Ipsum is simply
-          dummy text of the printing and typesetting industry. Lorem Ipsum has
-          been the industry standard dummy text ever since the 1500s, when an
-          unknown printer took a galley of type and scrambled it to make a type
-          specimen book. It has survived not only five centuries, but also the
-          leap into electronic typesetting, remaining essentially unchanged.
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry standard dummy text ever
-          since the 1500. Lorem Ipsum is simply dummy text of the printing and
-          typesetting industry. Lorem Ipsum has been the industry standard dummy
-          text ever since the 1500s, when an unknown printer took a galley of
-          type and scrambled it to make a type specimen book. It has survived
-          not only five centuries, but also the leap into electronic
-          typesetting, remaining essentially unchanged. Lorem Ipsum is simply
-          dummy text of the printing and typesetting industry. Lorem Ipsum has
-          been the industry standard dummy text ever since the 1500s, when an
-          unknown printer took a galley of type and scrambled it to make a type
-          specimen book. It has survived not only five centuries, but also the
-          leap into electronic typesetting, remaining essentially unchanged.
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. Lorem Ipsum is simply dummy text of
-          the printing and typesetting industry. Lorem Ipsum has been the
-          industry standard dummy text ever since the 1500s, when an unknown
-          printer took a galley of type and scrambled it to make a type specimen
-          book. It has survived not only five centuries, but also the leap into
-          electronic typesetting, remaining essentially unchanged. Lorem Ipsum
-          is simply dummy text of the printing and typesetting industry. Lorem
-          Ipsum has been the industry standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it to make
-          a type specimen book. It has survived not only five centuries, but
-          also the leap into electronic typesetting, remaining essentially
-          unchanged. Lorem Ipsum is simply dummy text of the printing and
-          typesetting industry. Lorem Ipsum has been the industry standard dummy
-          text ever since the 1500.
-        </span>
+        <div className="space-y-5">
+          <p className="flex-1">
+            Meu nome é Erik de Souza, sou Desenvolvedor Frontend e moro no
+            Brasil. Sou fascinado pela stack do Javascript e estou sempre
+            buscando evoluir diariamente. Atualmente estou no 5º semestre de
+            Sistemas da Informação e já trabalhei por quase dois anos no
+            desenvolvimento web e mobile.
+          </p>
+
+          {/*  */}
+          <p className="flex-1">
+            Minha jornada no desenvolvimento começou enquanto cursava o técnico
+            em Informática na EEEP Jaime Alencar de Oliveira, onde tive a
+            oportunidade de estagiar na Softtek. Durante esse período, comecei
+            aprimorando habilidades em HTML, CSS e JavaScript, além de trabalhar
+            na geração de relatórios dos chamados atendidos pela equipe.
+          </p>
+          {/*  */}
+          <p className="flex-1">
+            Após o estagio, trabalhei como Desenvolvedor Frontend na Tecla T,
+            onde pude participar de projetos como e-commerce, apps mobile e
+            painéis administrativos, implementando soluções de integração com
+            APIs, fluxos de carrinho de compra e layout fluido para oferecer uma
+            boa experiência para os usuários.
+          </p>
+          {/*  */}
+          <p className="flex-1">
+            Participei do programa Ignite pela Rocketseat, onde pude concluir a
+            trilha de ReactJS. Ao longo do curso, criei projetos desafiadores
+            que me renderam muito aprendizado, alguns desses projetos foram o
+            Book Wise, um sistema de avaliação de livros utilizando Next.js e
+            MySQL, e o Ignite Call, um projeto para efetuar marcação de chamadas
+            e reuniões, integrando o Next.js à API do Google Calendar. Com isso
+            pude ir além da competência técnica, mostrando capacidade de
+            enfrentar desafios complexos de forma criativa.
+          </p>
+          {/*  */}
+          <p className="flex-1">
+            Meus conhecimentos não se limitam apenas às tecnologias mais
+            recentes, como React, Next.js, TailwindCSS e Stitches. Também sou
+            adepto de metodologias ágeis, como Scrum, e tenho experiência no
+            versionamento de código usando GitFlow, garantindo a qualidade e a
+            colaboração eficiente em cada projeto.
+          </p>
+          {/*  */}
+          <p className="flex-1">
+            Estou constantemente em busca de novos desafios que me permitam
+            continuar aprendendo e crescendo como desenvolvedor. Seja para criar
+            interfaces intuitivas ou desenvolver soluções inovadoras, estou
+            comprometido em impulsionar a excelência por meio do código.
+          </p>
+          {/*  */}
+          <p className="flex-1">
+            Se você está em busca de um desenvolvedor comprometido, apaixonado
+            por solucionar problemas e apaixonado por tecnologia, estou pronto
+            para colaborar e tornar seus projetos realidade!
+          </p>
+        </div>
       </article>
     </main>
   )
