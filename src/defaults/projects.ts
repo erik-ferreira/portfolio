41c1fe6cfab3fa0a77350dfaa8dbc477105fa512
erik-ferreira <erikdesouzagf@gmail.com>
2024-01-22 22:47:57 -0300

    add resend to work emails to contact

--- src/defaults/projects.ts
+++ src/defaults/projects.ts
@@ -60,7 +60,7 @@ const projectsDefault: ProjectsDefault[] = [
   {
     title: "Ignite Shop",
     description:
-      "Um projeto que surgiu durante o Módulo 04 do Ignite da Rocketseat e inicialmente, concentrei-me na criação de uma conexão com o Stripe para buscar e gerenciar produtos, especificamente camisetas, enquanto explorava os fundamentos do desenvolvimento. Utilizei Next.js como base para o projeto, aproveitando o poder do StitchesJS para toda a estilização, garantindo uma interface atraente e responsiva. A implementação do checkout de pagamento foi realizada de maneira eficiente utilizando a API do Stripe. Após concluir as aulas do módulo, abracei desafios adicionais para aprimorar o projeto, incluindo: - Aperfeiçoamentos no layout para proporcionar uma experiência ainda mais cativante; - Implementação do fluxo completo de um carrinho de compras, possibilitando uma experiência de compra fluida; - Desenvolvimento de uma funcionalidade que permite o checkout de pagamento com múltiplos produtos, integrando-os perfeitamente à API do Stripe.",
+      "Um projeto que surgiu durante o Módulo 04 do Ignite da Rocketseat e inicialmente, concentrei-me na criação de uma conexão com o Stripe para buscar e gerenciar produtos, especificamente camisetas, enquanto explorava os fundamentos do desenvolvimento. Utilizei Next.js como base para o projeto, aproveitando o poder do StitchesJS para toda a estilização, garantindo uma interface atraente e responsiva. A implementação do checkout de pagamento foi realizada de maneira eficiente utilizando a API do Stripe. Após concluir as aulas do módulo, abracei desafios adicionais para aprimorar o projeto, incluindo: aperfeiçoamentos no layout para proporcionar uma experiência ainda mais cativante, implementação do fluxo completo de um carrinho de compras, possibilitando uma experiência de compra fluida e o desenvolvimento de uma funcionalidade que permite o checkout de pagamento com múltiplos produtos, integrando-os perfeitamente à API do Stripe.",
     src: "/projects/ignite-shop/default.png",
     skills: formatSkillsProject([
       {
@@ -102,7 +102,7 @@ const projectsDefault: ProjectsDefault[] = [
   {
     title: "Coffee Delivery",
     description:
-      "Um projeto desenvolvido como um desafio do módulo de React no Ignite da Rocketseat. Este projeto visa gerenciar o carrinho de compras de uma cafeteria fictícia, oferecendo uma gama de funcionalidades para uma experiência de compra aprimorada. Ao longo do desenvolvimento, concentrei-me em diversos aspectos-chave: Listagem detalhada de produtos (especificamente cafés) disponíveis para compra, oferecendo aos usuários uma variedade de opções; Capacidade de adicionar uma quantidade específica de itens ao carrinho, permitindo uma experiência de compra personalizada; Flexibilidade para aumentar ou remover a quantidade de itens no carrinho, adaptando-se às necessidades dos usuários; Implementação de um formulário intuitivo para coleta de informações de endereço dos usuários, aprimorando a usabilidade do serviço; Exibição dinâmica do total de itens no carrinho no Header, proporcionando uma visão clara do que está sendo adquirido; Cálculo automático do valor total dos itens no carrinho, multiplicando a quantidade pelo valor unitário dos produtos selecionados; Emprego extensivo do Tailwind CSS para estilização e responsividade em todo o projeto, garantindo uma interface atraente e adaptável; Utilização do React Hook Form para facilitar a manipulação de formulários, e a biblioteca Zod para validação de dados, garantindo a integridade das informações.",
+      "Um projeto desenvolvido como um desafio do módulo de React no Ignite da Rocketseat. Este projeto visa gerenciar o carrinho de compras de uma cafeteria fictícia, oferecendo uma gama de funcionalidades para uma experiência de compra aprimorada. Ao longo do desenvolvimento, concentrei-me em diversos aspectos-chave como listagem detalhada de produtos (especificamente cafés) disponíveis para compra, oferecendo aos usuários uma variedade de opções; Capacidade de adicionar uma quantidade específica de itens ao carrinho, permitindo uma experiência de compra personalizada; Flexibilidade para aumentar ou remover a quantidade de itens no carrinho, adaptando-se às necessidades dos usuários; Implementação de um formulário intuitivo para coleta de informações de endereço dos usuários, aprimorando a usabilidade do serviço; Exibição dinâmica do total de itens no carrinho no Header, proporcionando uma visão clara do que está sendo adquirido; Cálculo automático do valor total dos itens no carrinho, multiplicando a quantidade pelo valor unitário dos produtos selecionados; Emprego extensivo do Tailwind CSS para estilização e responsividade em todo o projeto, garantindo uma interface atraente e adaptável; Utilização do React Hook Form para facilitar a manipulação de formulários, e a biblioteca Zod para validação de dados, garantindo a integridade das informações.",
     src: "/projects/coffee-delivery/default.png",
     skills: formatSkillsProject([
       {
@@ -175,7 +175,7 @@ const projectsDefault: ProjectsDefault[] = [
       },
     ]),
     github_href: "https://github.com/erik-ferreira/reminder-calendar",
-    demo_href: null,
+    demo_href: "https://reminder-calendar-virid.vercel.app",
     screenshots: Array.from({ length: 3 }, (_, index) => {
       const imageIndex = String(index + 1).padStart(2, "0")
 
@@ -192,7 +192,7 @@ const projectsDefault: ProjectsDefault[] = [
   {
     title: "Book Wise",
     description:
-      "Este projeto foi construído com Next.js e faz o gerenciamento de leituras, contando com avaliações dos livros. A construção do frontend foi feita toda em cima do Next 13, e além disso pude desenvolver o backend com prisma, banco de dados mysql com docker. Toda a estilização foi feita com tailwind e além das telas para web, adicionei responsividade em toda a aplicação, para que seja possível acessar do mobile sem problema algum.",
+      "Este projeto foi construído com Next.js e faz o gerenciamento de leituras, contando com avaliações dos livros. A construção do frontend foi feita toda em cima do Next 13, e além disso pude desenvolver o backend com prisma e banco de dados mysql rodando no container do docker. Toda a estilização foi feita com tailwind e além das telas para web, adicionei responsividade em toda a aplicação, para que seja possível acessar do mobile sem problema algum.",
     src: "/projects/book-wise/default.png",
     skills: formatSkillsProject([
       {
@@ -530,7 +530,7 @@ const projectsDefault: ProjectsDefault[] = [
       },
     ]),
     github_href: "https://github.com/erik-ferreira/challenge-03-github-blog",
-    demo_href: null,
+    demo_href: "https://challenge-03-github-blog.vercel.app",
     screenshots: Array.from({ length: 3 }, (_, index) => {
       const imageIndex = String(index + 1).padStart(2, "0")
 
@@ -547,7 +547,7 @@ const projectsDefault: ProjectsDefault[] = [
   {
     title: "Mastering Tailwind",
     description:
-      "Este projeto tem como objetivo construir uma aplicação completa e responsiva com Tailwindcss. A aplicação não tem um objetivo comercial em si, mas possui vários componentes que são usados no dia a dia de várias aplicações e é incrível poder construir todos eles com muita facilidade e rapidez, já que o Tailwind consegue proporcionar isso. Alguns dos componentes da aplicação são: - Preview de foto de perfil; - Navbar; - Select; - Input",
+      "Este projeto tem como objetivo construir uma aplicação completa e responsiva com Tailwindcss. A aplicação não tem um objetivo comercial em si, mas possui vários componentes que são usados no dia a dia de várias aplicações e é incrível poder construir todos eles com muita facilidade e rapidez, já que o Tailwind consegue proporcionar isso. Alguns dos componentes da aplicação são: Preview de foto de perfil, navbar, select, input e etc",
     src: "/projects/mastering-tailwind/default.png",
     skills: formatSkillsProject([
       {
