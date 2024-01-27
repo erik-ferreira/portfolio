4d34de93b63aab99123d92e56e744ddc1249f472
erik-ferreira <erikdesouzagf@gmail.com>
2024-01-26 21:11:08 -0300

    complete style email template

--- src/components/EmailTemplate.tsx
+++ src/components/EmailTemplate.tsx
@@ -3,37 +3,45 @@ import { CSSProperties } from "react"
 interface StylesProps {
   container: CSSProperties
   title: CSSProperties
-  labelBackground: CSSProperties
   label: CSSProperties
+  subtitle: CSSProperties
+  description: CSSProperties
 }
 
 export const styles = {
   container: {
     backgroundColor: "#090b0e", // bg-page
+    paddingBottom: 32,
   },
   title: {
-    position: "relative",
-  },
-  labelBackground: {
-    display: "block",
-    fontSize: "5rem",
-    fontFamily: "Orbitron",
-    fontWeight: "semibold",
+    color: "#1e293b",
+
+    fontSize: "6rem",
+    fontWeight: 600,
     textTransform: "uppercase",
+
     textAlign: "center",
     filter: "blur(2px)",
+    margin: 0,
   },
-  label: {
-    fontFamily: "Permanent Marker",
-    fontSize: "3.5rem",
-    color: "transparent",
-    backgroundColor: "linear-gradient(to bottom, #3b82f6, #7c3aed)",
-    backgroundClip: "text",
+  subtitle: {
+    color: "#843CE5", // bg-violet-400
+    fontSize: "2rem",
+    fontWeight: 700,
 
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
+    textAlign: "center",
+    margin: 0,
+  },
+  description: {
+    width: 632,
+    margin: "0 auto",
+
+    color: "#E2E8F0", // bg-slate-200
+
+    fontSize: "1.125rem",
+    fontWeight: 700,
+
+    textAlign: "center",
   },
 } as StylesProps
 
@@ -44,14 +52,11 @@ interface EmailTemplateProps {
 export function EmailTemplate({ firstName }: EmailTemplateProps) {
   return (
     <div style={styles.container}>
-      <div style={styles.title}>
-        <span style={styles.labelBackground}>Portfolio</span>
-        <h1 style={styles.label}>Portfólio</h1>
-      </div>
+      <h1 style={styles.title}>Portfólio</h1>
 
-      <h2>Olá Erik, me chamo Rafael!</h2>
+      <h2 style={styles.subtitle}>Olá Erik, me chamo Rafael!</h2>
 
-      <p>
+      <p style={styles.description}>
         Uma breve descrição bem aqui Uma breve descrição bem aqui a breve
         descrição bem aqui Uma breve descrição bem aqui Uma ve descrição bem
         aqui Uma breve descrição bem aqui Uma dale a a bem aqui
