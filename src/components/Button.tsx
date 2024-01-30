573111ff0d00a5a06a4a7b859a3d9bd9c42f96e4
erik-ferreira <erikdesouzagf@gmail.com>
2024-01-29 22:15:46 -0300

    add loading in button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,20 @@ import { Slot } from "@radix-ui/react-slot"
 import { ButtonHTMLAttributes } from "react"
 import { tv, VariantProps } from "tailwind-variants"
 
+import { LoadingSpinner } from "@/components/LoadingSpinner"
+
 const button = tv({
   base: "w-fit flex items-center justify-center gap-2 px-4 py-3 rounded transition-colors duration-400 font-bold disabled:opacity-60 disabled:pointer-events-none",
 
   variants: {
     variant: {
       default: "bg-blue-600 enabled:hover:bg-blue-700",
-      outline:
-        "bg-transparent border-2 border-blue-500 text-blue-500 shadow-blue hover:bg-blue-500 hover:text-page",
+      outline: [
+        "bg-transparent text-blue-500 shadow-blue",
+        "border-2 border-blue-500",
+        "hover:bg-blue-500 hover:text-page",
+        "data-[loading=true]:w-32",
+      ],
       "outline-gradient": "bg-page hover:bg-transparent",
     },
 
@@ -28,6 +34,7 @@ const button = tv({
 type ButtonProps = VariantProps<typeof button> &
   ButtonHTMLAttributes<HTMLButtonElement> & {
     asChild?: boolean
+    loading?: boolean
   }
 
 export function Button({
@@ -35,17 +42,23 @@ export function Button({
   size,
   className,
   asChild = false,
-
+  loading = false,
   ...rest
 }: ButtonProps) {
   const Component = asChild ? Slot : "button"
   const classNameComponent = button({ variant, size, className })
 
+  const componentButton = (
+    <Component data-loading={loading} className={classNameComponent} {...rest}>
+      {loading ? <LoadingSpinner /> : rest.children}
+    </Component>
+  )
+
   return variant === "outline-gradient" ? (
     <div className="w-fit p-0.5 rounded bg-gradient-to-r from-blue-500 to-violet-600 to-80%">
-      <Component className={classNameComponent} {...rest} />
+      {componentButton}
     </div>
   ) : (
-    <Component className={classNameComponent} {...rest} />
+    <>{componentButton}</>
   )
 }
---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,28 @@
+import { ComponentProps } from "react"
+import { tv, VariantProps } from "tailwind-variants"
+
+const loadingSpinner = tv({
+  base: "animate-spin rounded-full border-2 border-blue-500 border-b-transparent",
+
+  variants: {
+    size: {
+      default: "w-6 h-6",
+      large: "w-10 h-10",
+    },
+  },
+
+  defaultVariants: {
+    size: "default",
+  },
+})
+
+type LoadingSpinnerProps = VariantProps<typeof loadingSpinner> &
+  ComponentProps<"span"> & {}
+
+export function LoadingSpinner({
+  className,
+  size,
+  ...rest
+}: LoadingSpinnerProps) {
+  return <span className={loadingSpinner({ className, size })} {...rest} />
+}
